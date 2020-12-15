89e869e674bfe2db496e63ef40d8b6d7bc9fa77d
SeDB1914 <SeDB1914@users.noreply.github.com>
2020-12-14 22:24:01 -0600

    heroes tutorial near completion

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Dr Nice', power: "Smiles", weakness: "infant's crying" },
+  { id: 12, name: 'Narco', power: "immune to all drugs", weakness: "" },
+  { id: 13, name: 'Bombasto', power: "", weakness: "extreme cold" },
+  { id: 14, name: 'Celeritas', power: "grow plant cells at will", weakness: "Vegans" },
+  { id: 15, name: 'Magneta', power: "magnetism", weakness: "plastic" },
+  { id: 16, name: 'RubberMan', power: "elasticity", weakness: "extreme heat" },
+  { id: 17, name: 'Dynama', power: "", weakness: "" },
+  { id: 18, name: 'Dr IQ', power: "hypnosis", weakness: "" },
+  { id: 19, name: 'Magma', power: "volcanic eruption", weakness: "" },
+  { id: 20, name: 'Tornado', power: "manipulates wind", weakness: "helium" }
+];
